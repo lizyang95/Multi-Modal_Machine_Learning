aad3c81f48e2deffdd0dfa665843dddf40ca11ff
lizyang95 <yy4@cs.cmu.edu>
2018-11-26 23:51:44 -0500

    load embedding

--- VisualTranslation/language/pretrained_embedding.py
+++ VisualTranslation/language/pretrained_embedding.py
@@ -15,6 +15,38 @@ NUMBER_OF_DIMENSIONS = 25
 SUBFOLDER_NAME = "glove.twitter.27B"
 
 
+# download Twitter.25d embeddings from Standford
+DATA_FOLDER = "embeddings"
+ZIP_FILE = os.path.join(DATA_FOLDER, "{}.zip".format(SUBFOLDER_NAME))
+ZIP_FILE_ALT = "glove" + ZIP_FILE[5:]  # sometimes it's lowercase only...
+UNZIP_FOLDER = os.path.join(DATA_FOLDER, SUBFOLDER_NAME)
+if SUBFOLDER_NAME[-1] == "d":
+    GLOVE_FILENAME = os.path.join(UNZIP_FOLDER, "{}.txt".format(SUBFOLDER_NAME))
+else:
+    GLOVE_FILENAME = os.path.join(UNZIP_FOLDER, "{}.{}d.txt".format(SUBFOLDER_NAME, NUMBER_OF_DIMENSIONS))
+
+
+if not os.path.exists(ZIP_FILE) and not os.path.exists(UNZIP_FOLDER):
+    # GloVe by Stanford is licensed Apache 2.0:
+    #     https://github.com/stanfordnlp/GloVe/blob/master/LICENSE
+    #     http://nlp.stanford.edu/data/glove.twitter.27B.zip
+    #     Copyright 2014 The Board of Trustees of The Leland Stanford Junior University
+    print("Downloading embeddings to '{}'".format(ZIP_FILE))
+    chakin.download(number=CHAKIN_INDEX, save_dir='./{}'.format(DATA_FOLDER))
+else:
+    print("Embeddings already downloaded.")
+
+if not os.path.exists(UNZIP_FOLDER):
+    import zipfile
+    if not os.path.exists(ZIP_FILE) and os.path.exists(ZIP_FILE_ALT):
+        ZIP_FILE = ZIP_FILE_ALT
+    with zipfile.ZipFile(ZIP_FILE,"r") as zip_ref:
+        print("Extracting embeddings to '{}'".format(UNZIP_FOLDER))
+        zip_ref.extractall(UNZIP_FOLDER)
+else:
+    print("Embeddings already extracted.")
+
+
 def load_embedding_from_disks(glove_filename, with_indexes=True):
     """
     Read a GloVe txt file. If `with_indexes=True`, we return a tuple of two dictionnaries
@@ -55,38 +87,3 @@ def load_embedding_from_disks(glove_filename, with_indexes=True):
     else:
         word_to_embedding_dict = defaultdict(lambda: _WORD_NOT_FOUND)
         return word_to_embedding_dict
-
-
-print("Loading embedding from disks...")
-word_to_index, index_to_embedding = load_embedding_from_disks(GLOVE_FILENAME, with_indexes=True)
-print("Embedding loaded from disks.")
-# download Twitter.25d embeddings from Standford
-# DATA_FOLDER = "embeddings"
-# ZIP_FILE = os.path.join(DATA_FOLDER, "{}.zip".format(SUBFOLDER_NAME))
-# ZIP_FILE_ALT = "glove" + ZIP_FILE[5:]  # sometimes it's lowercase only...
-# UNZIP_FOLDER = os.path.join(DATA_FOLDER, SUBFOLDER_NAME)
-# if SUBFOLDER_NAME[-1] == "d":
-#     GLOVE_FILENAME = os.path.join(UNZIP_FOLDER, "{}.txt".format(SUBFOLDER_NAME))
-# else:
-#     GLOVE_FILENAME = os.path.join(UNZIP_FOLDER, "{}.{}d.txt".format(SUBFOLDER_NAME, NUMBER_OF_DIMENSIONS))
-#
-#
-# if not os.path.exists(ZIP_FILE) and not os.path.exists(UNZIP_FOLDER):
-#     # GloVe by Stanford is licensed Apache 2.0:
-#     #     https://github.com/stanfordnlp/GloVe/blob/master/LICENSE
-#     #     http://nlp.stanford.edu/data/glove.twitter.27B.zip
-#     #     Copyright 2014 The Board of Trustees of The Leland Stanford Junior University
-#     print("Downloading embeddings to '{}'".format(ZIP_FILE))
-#     chakin.download(number=CHAKIN_INDEX, save_dir='./{}'.format(DATA_FOLDER))
-# else:
-#     print("Embeddings already downloaded.")
-#
-# if not os.path.exists(UNZIP_FOLDER):
-#     import zipfile
-#     if not os.path.exists(ZIP_FILE) and os.path.exists(ZIP_FILE_ALT):
-#         ZIP_FILE = ZIP_FILE_ALT
-#     with zipfile.ZipFile(ZIP_FILE,"r") as zip_ref:
-#         print("Extracting embeddings to '{}'".format(UNZIP_FOLDER))
-#         zip_ref.extractall(UNZIP_FOLDER)
-# else:
-#     print("Embeddings already extracted.")

a7c98893ae1be60c62a67ee5f87af97bdc32f348
lizyang95 <yy4@cs.cmu.edu>
2018-11-27 01:01:28 -0500

    debugging

--- VisualTranslation/language/word_similarity.py
+++ VisualTranslation/language/word_similarity.py
@@ -67,7 +67,7 @@ def cosine_similarity_tensorflow(tf_word_representation_A, tf_words_representati
 
     return similarity
 
-    
+
 tf.reset_default_graph()
 
 
@@ -123,7 +123,7 @@ nb_similar_words_to_get = 10
 
 
 word = "king"
-word_id = word_to_index[word]
+word_id = tf_word_to_index[word]
 
 top_cosine_similarities, top_word_indices, top_words_representation = sess.run(
     [tf_top_cosine_similarities, tf_top_word_indices, tf_top_words_representation],
